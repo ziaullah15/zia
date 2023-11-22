b2c9861b8644e8f2bda96cbd46ae17d732a959eb
Zia Ullah <105817603+ziaullah15@users.noreply.github.com>
2023-11-22 13:42:17 +0500

    Add files via upload

---
+++ moonpay.js
@@ -0,0 +1,16 @@
+function helo() {
+    const moonpaySdk = window.MoonPayWebSdk.init({
+        flow: 'nft',
+        environment: 'sandbox',
+        variant: 'overlay',
+        params: {
+          apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
+          contractAddress: '0x28Aa3f5FF8C171dCF7E1F5aF03A8E1C1c3411c73',
+          tokenId: '1'
+        }
+      });
+
+      console.log("hiiiiiiiiiii")
+
+      moonpaySdk.show()
+}

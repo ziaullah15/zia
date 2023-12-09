75c073b9f9ff2c5982bdf8b474eb183771c9bc60
Zia Ullah <105817603+ziaullah15@users.noreply.github.com>
2023-12-09 17:42:09 +0500

    Update moonpay.js

--- moonpay.js
+++ moonpay.js
@@ -5,7 +5,7 @@ function helo() {
         variant: 'overlay',
         params: {
           apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
-          contractAddress: '0xD28f604CE3152177B4265c902c29f6c69F11A7AB',
+          contractAddress: '0xc1Fb3E464cF39d9f0aCfB40831bee4E90b5637c7',
           tokenId: '1'
         }
       });

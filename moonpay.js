19aa05730e970c474f2c3228aa28012a768cb35e
Zia Ullah <105817603+ziaullah15@users.noreply.github.com>
2023-12-12 11:44:57 +0500

    Update moonpay.js

--- moonpay.js
+++ moonpay.js
@@ -2,32 +2,32 @@ function helo() {
 
         console.log("hiiiiiiiiiii");
 
-    const moonpaySdk = window.MoonPayWebSdk.init({
-        flow: 'nft',
-        environment: 'sandbox',
-        variant: 'overlay',
-        params: {
-          apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
-          contractAddress: '0xc1Fb3E464cF39d9f0aCfB40831bee4E90b5637c7',
-          tokenId: '1',
-          dynamicAssetInfo: 'true'
-        }
-    });
+    // const moonpaySdk = window.MoonPayWebSdk.init({
+    //     flow: 'nft',
+    //     environment: 'sandbox',
+    //     variant: 'overlay',
+    //     params: {
+    //       apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
+    //       contractAddress: '0xc1Fb3E464cF39d9f0aCfB40831bee4E90b5637c7',
+    //       tokenId: '1',
+    //       dynamicAssetInfo: 'true'
+    //     }
+    // });
 
-    const urlForSignature = moonpaySdk.generateUrlForSigning();
+    // const urlForSignature = moonpaySdk.generateUrlForSigning();
 
-    // The URL for the signature should be sent to your backend, which should then
-    // sign it with your API secret and return the signature.
-    const response = await fetch("/sign-url", {
-        method: "POST",
-        body: JSON.stringify({ urlForSignature }),
-    });
-    const data = await response.json();
+    // // The URL for the signature should be sent to your backend, which should then
+    // // sign it with your API secret and return the signature.
+    // const response = await fetch("/sign-url", {
+    //     method: "POST",
+    //     body: JSON.stringify({ urlForSignature }),
+    // });
+    // const data = await response.json();
 
-    // Once you have the signature, you can update the SDK with it and show the
-    // widget.
-    moonpaySdk.updateSignature(data.signature);
+    // // Once you have the signature, you can update the SDK with it and show the
+    // // widget.
+    // moonpaySdk.updateSignature(data.signature);
 
 
-    moonpaySdk.show();
+    // moonpaySdk.show();
 }

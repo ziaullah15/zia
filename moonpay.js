2fcb38f1e73c055ccbea9707b9b1c612b69bde1e
Zia Ullah <105817603+ziaullah15@users.noreply.github.com>
2023-12-12 11:45:56 +0500

    Update moonpay.js

--- moonpay.js
+++ moonpay.js
@@ -2,17 +2,17 @@ function helo() {
 
         console.log("hiiiiiiiiiii");
 
-    // const moonpaySdk = window.MoonPayWebSdk.init({
-    //     flow: 'nft',
-    //     environment: 'sandbox',
-    //     variant: 'overlay',
-    //     params: {
-    //       apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
-    //       contractAddress: '0xc1Fb3E464cF39d9f0aCfB40831bee4E90b5637c7',
-    //       tokenId: '1',
-    //       dynamicAssetInfo: 'true'
-    //     }
-    // });
+    const moonpaySdk = window.MoonPayWebSdk.init({
+        flow: 'nft',
+        environment: 'sandbox',
+        variant: 'overlay',
+        params: {
+          apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
+          contractAddress: '0xc1Fb3E464cF39d9f0aCfB40831bee4E90b5637c7',
+          tokenId: '1',
+          dynamicAssetInfo: 'true'
+        }
+    });
 
     // const urlForSignature = moonpaySdk.generateUrlForSigning();
 
@@ -29,5 +29,5 @@ function helo() {
     // moonpaySdk.updateSignature(data.signature);
 
 
-    // moonpaySdk.show();
+    moonpaySdk.show();
 }

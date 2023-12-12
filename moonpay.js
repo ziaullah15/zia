e926bb5d031dfb35146499b2566af147abc30355
Zia Ullah <105817603+ziaullah15@users.noreply.github.com>
2023-12-12 11:30:20 +0500

    Update moonpay.js

--- moonpay.js
+++ moonpay.js
@@ -6,9 +6,23 @@ function helo() {
         params: {
           apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
           contractAddress: '0xc1Fb3E464cF39d9f0aCfB40831bee4E90b5637c7',
-          quantity: '1',
+          tokenId: '1',
           dynamicAssetInfo: 'true'
         }
+        const urlForSignature = moonpaySdk.generateUrlForSigning();
+
+// The URL for signature should be sent to your backend, which should then
+// sign it with your API secret and return the signature.
+const response = await fetch("/sign-url", {
+  method: "POST",
+  body: JSON.stringify({ urlForSignature }),
+});
+const data = await response.json();
+
+// Once you have the signature, you can update the SDK with it and show the
+// widget.
+moonpaySdk.updateSignature(data.signature);
+        
       });
 
       console.log("hiiiiiiiiiii")

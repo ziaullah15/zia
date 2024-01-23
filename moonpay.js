6b4c025e643b5e8607d3c899e0064237c025194a
Zia Ullah <105817603+ziaullah15@users.noreply.github.com>
2024-01-23 18:03:04 +0500

    Update moonpay.js

--- moonpay.js
+++ moonpay.js
@@ -1,17 +1,17 @@
 async function helo() {
 
         console.log("hiiiiiiiiiii");
-        // const metadata = encodeURIComponent(JSON.stringify({
-        //   moonpayCollectionId: "48c1d2e0-cb31-4c6d-94e6-c74784ba3b7b",
-        //   mintData: {
-        //     _maxQuantity: 5,  // Key name has to match argument name in the mint function!!
-        //   }
-        // }))
+        const metadata = encodeURIComponent(JSON.stringify({
+            moonpayCollectionId: "49d10973-9a10-4c3c-ae48-555aa345c8e6",
+            mintData: {
+              _maxQuantity: 5,  // Key name has to match argument name in the mint function!!
+            }
+          }))
     const moonpaySdk = window.MoonPayWebSdk.init({
-        debug: true,
         flow: 'nft',
         environment: 'sandbox',
         variant: 'overlay',
+        metadata: metadata,
         params: {
           apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
           contractAddress: '0x2B072BF5647f72f786Fd1aE213bcF7Af078fd513',          
@@ -21,6 +21,7 @@ async function helo() {
 
     const urlForSignature = moonpaySdk.generateUrlForSigning();
     console.log("url:---",urlForSignature)
+    debugger
     // The URL for the signature should be sent to your backend, which should then
     // sign it with your API secret and return the signature.
     const response = await fetch("https://api-dev.vtail.com/api/moonpay/getSignature", {

603cc8ef84a02877533f4fd2ec4e016c58282d1d
Zia Ullah <105817603+ziaullah15@users.noreply.github.com>
2024-01-25 12:13:40 +0500

    Update moonpay.js

--- moonpay.js
+++ moonpay.js
@@ -1,27 +1,30 @@
 async function helo() {
 
         console.log("hiiiiiiiiiii");
-        const metadata = encodeURIComponent(JSON.stringify({
-            moonpayCollectionId: "49d10973-9a10-4c3c-ae48-555aa345c8e6",
-            mintData: {
-              _maxQuantity: 5,  // Key name has to match argument name in the mint function!!
-            }
-          }))
+        // const metadata = encodeURIComponent(JSON.stringify({
+        //     moonpayCollectionId: "49d10973-9a10-4c3c-ae48-555aa345c8e6",
+        //     mintData: {
+        //       _maxQuantity: 5,  // Key name has to match argument name in the mint function!!
+        //     }
+        //   }))
     const moonpaySdk = window.MoonPayWebSdk.init({
+        debug: true,
         flow: 'nft',
         environment: 'sandbox',
-        variant: 'overlay',
-        metadata: metadata,
+        variant: 'newTab',
         params: {
           apiKey: 'pk_test_9l5tjCkdWZ66fgEOtLJ2wih6Zg3t4DeE',
-          contractAddress: '0x2B072BF5647f72f786Fd1aE213bcF7Af078fd513',          
-          tokenId: '_0'
+          contractAddress: '0x65603C5bc4bb1aD489F63C91d19D8c58722c2e7e',          
+          tokenId: '_0',
+          metadata: {
+            moonpayCollectionId: 'fa64b2f3-a88e-495c-92bb-70c8c3229eeb',
+        }
         }
     });
 
     const urlForSignature = moonpaySdk.generateUrlForSigning();
     console.log("url:---",urlForSignature)
-    debugger
+    // debugger
     // The URL for the signature should be sent to your backend, which should then
     // sign it with your API secret and return the signature.
     const response = await fetch("https://api-dev.vtail.com/api/moonpay/getSignature", {

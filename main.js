fea2b871cde2d358fcf1977f57703ddb84e1beb8
ziaullah15 <webspider1198@gmail.com>
2026-01-29 15:37:33 +0500

    updated connection

---
+++ main.js
@@ -0,0 +1,42 @@
+import { createAppKit } from '@reown/appkit'
+import { mainnet, arbitrum } from '@reown/appkit/networks'
+import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
+
+// 1. Get a project ID at https://dashboard.reown.com
+const projectId = 'abbdaff37b3fab2c0dc3cd248cb47cf7'
+
+export const networks = [mainnet, arbitrum]
+
+// 2. Set up Wagmi adapter
+const wagmiAdapter = new WagmiAdapter({
+  projectId,
+  networks
+})
+
+// 3. Configure the metadata
+const metadata = {
+  name: 'AppKit',
+  description: 'AppKit Example',
+  url: 'https://zia-orcin.vercel.app/', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/179229932']
+}
+
+// 3. Create the modal
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  networks: [mainnet, arbitrum],
+  metadata,
+  projectId,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
+// 4. Trigger modal programaticaly
+const openConnectModalBtn = document.getElementById('open-connect-modal')
+const openNetworkModalBtn = document.getElementById('open-network-modal')
+
+openConnectModalBtn.addEventListener('click', () => modal.open())
+openNetworkModalBtn.addEventListener('click', () => modal.open({ view: 'Networks' }))
+
+// 5. Alternatively use w3m component buttons within the index.html file
